ce9ff0582047bac0c7cc9f384b09ed59c1fa9d72
Andrew Drozdov <andrew@mrdrozdov.com>
2021-01-24 21:48:04 -0500

    Script for allrank data.

---
+++ build_allrank_data.py
@@ -0,0 +1,140 @@
+import argparse
+import collections
+import os
+
+import numpy as np
+
+from tqdm import tqdm
+
+
+def main(args):
+    np.random.seed(args.seed)
+
+    dstore = Dstore(args.dstore, args.dstore_size, 1024)
+    dstore.initialize()
+    dstore.add_neighbors(args.lookup, args.lookup_k)
+
+    dsize = dstore.keys.shape[0]
+
+    # get splits
+    index = np.arange(dsize)
+    np.random.shuffle(index)
+
+    splits = [
+        ('train', args.ntrain),
+        ('valid', args.nvalid),
+    ]
+
+    print('mkdir -p {}'.format(args.output))
+    os.system('mkdir -p {}'.format(args.output))
+
+    offset = 0
+    for name, size in splits:
+        path = os.path.join(args.output, 'allrank_{}.txt'.format(name))
+        local_index = index[offset:offset+size]
+        tgts = dstore.tgts[local_index]
+        knns = dstore.knns[local_index][:, :args.k]
+        # dist is sorted from hi to lo
+        dist = dstore.dist[local_index][:, :args.k]
+        knn_tgts = dstore.tgts[knns.reshape(-1)].reshape(size, args.k, 1)
+
+        # get qids
+        qids = local_index
+
+        # get features
+        feat = dstore.keys[knns.reshape(-1)].reshape(size, args.k, dstore.vec_size)
+
+        # get label
+        label = (knn_tgts == tgts.reshape(-1, 1, 1)).astype(np.int)
+
+        del tgts
+        del knn_tgts
+        del knns
+
+        # get optimal order
+
+        ## positives - sort from hi to lo
+        positive_dist = dist.copy()
+        positive_dist[label == 0] = -np.inf
+        positive_order = positive_dist.argsort(axis=1)[:, ::-1]
+
+        ## negatives - sort from lo to hi
+        negative_dist = dist.copy()
+        negative_dist[label == 1] = -np.inf
+        negative_order = negative_dist.argsort(axis=1)
+
+        new_order = np.zeros((size, args.k, 1)).astype(np.int)
+        # set positives
+        new_order[positive_dist > -np.inf] = positive_order[positive_dist > -np.inf]
+        # set negatives
+        new_order[negative_dist > -np.inf] = negative_order[negative_dist > -np.inf]
+
+        assert np.all(np.unique(new_order, return_counts=True)[1] == size)
+
+        label = np.take_along_axis(label, new_order, axis=1)
+        feat = np.take_along_axis(feat, new_order, axis=1)
+
+        print('writing {} with feat shape {}'.format(path, feat.shape))
+        write_allrank_data(path, qids, label, feat)
+
+
+def format_vec(vec):
+    if not isinstance(vec, list):
+        assert len(vec.shape) == 1
+        vec = vec.tolist()
+    return ' '.join(['{}:{:.4f}'.format(i, x) for i, x in enumerate(vec)])
+
+
+def write_allrank_data(path, qids, label, feat):
+    size, k, _ = label.shape
+    with open(path, 'w') as f:
+        for i_slate in range(size):
+            for i_k in range(k):
+                q = qids[i_slate]
+                y = label[i_slate, i_k, 0]
+                vec = format_vec(feat[i_slate, i_k])
+                f.write('{} qid:{} {}'.format(y, q, vec))
+                f.write('\n')
+
+
+class Dstore:
+    def __init__(self, path, dstore_size=None, vec_size=None):
+        self.path = path
+        self.dstore_size = dstore_size
+        self.vec_size = vec_size
+        self._initialized = False
+
+    def initialize(self):
+        path = self.path
+        self.keys = np.memmap(os.path.join(path, 'dstore_keys.npy'), dtype=np.float32, mode='r', shape=(self.dstore_size, self.vec_size))
+        self.tgts = np.memmap(os.path.join(path, 'dstore_tgts.npy'), dtype=np.int, mode='r', shape=(self.dstore_size, 1))
+        self.vals = np.memmap(os.path.join(path, 'dstore_vals.npy'), dtype=np.int, mode='r', shape=(self.dstore_size, 1))
+        self.prob = np.memmap(os.path.join(path, 'dstore_prob.npy'), dtype=np.float32, mode='r', shape=(self.dstore_size, 1))
+        self._initialized = True
+
+    def add_neighbors(self, path, k):
+        self.knns = np.memmap(os.path.join(path, 'lookup_knns.npy'), dtype=np.int, mode='r', shape=(self.dstore_size, k, 1))
+        self.dist = np.memmap(os.path.join(path, 'lookup_dist.npy'), dtype=np.float32, mode='r', shape=(self.dstore_size, k, 1))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # dstore
+    parser.add_argument('--dstore', default='dstore_train', type=str)
+    parser.add_argument('--dstore-size', default=103225485, type=int)
+    # dstore neighbors
+    parser.add_argument('--lookup', default='lookup_train', type=str)
+    parser.add_argument('--lookup-k', default=128, type=int)
+    # allrank
+    parser.add_argument('--output', default=None, type=str)
+    parser.add_argument('--ntrain', default=1000, type=int)
+    parser.add_argument('--nvalid', default=100, type=int)
+    parser.add_argument('--seed', default=121, type=int)
+    parser.add_argument('--k', default=128, type=int)
+    parser.add_argument('--quant', action='store_true')
+    args = parser.parse_args()
+
+    print(args)
+
+    main(args)
+
