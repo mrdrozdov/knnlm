4a4e9cdc9833160784956d8575b61a0d73b81c57
Andrew Drozdov <andrew@mrdrozdov.com>
2021-01-29 22:12:15 -0500

    .

--- build_allrank_data.py
+++ build_allrank_data.py
@@ -15,20 +15,26 @@ def main(args):
     os.system('mkdir -p {}'.format(args.output))
 
     print('TRAIN')
-    out = build_split(args.tr_dstore, args.tr_dstore_size, args.tr_lookup, args.tr_lookup_k, args.k, args.ntrain, shuffle=not args.tr_noshuffle, balance=True)
+    out = build_split(args.tr_dstore, args.tr_dstore_size, args.tr_lookup, args.tr_lookup_k, args.k, args.ntrain,
+            shuffle=not args.tr_noshuffle, balance=args.tr_balance, should_filter_top=True)
     path = os.path.join(args.output, 'train.txt')
     write_allrank_data(path, out['qids'], out['label'], out['feat_idx'], out['q_src'], out['q_tgt'], out['feat_tgt'])
 
     print('VALID')
-    out = build_split(args.va_dstore, args.va_dstore_size, args.va_lookup, args.va_lookup_k, args.k, args.nvalid, shuffle=args.va_shuffle, balance=False)
+    out = build_split(args.va_dstore, args.va_dstore_size, args.va_lookup, args.va_lookup_k, args.k, args.nvalid,
+            shuffle=args.va_shuffle, balance=False)
     path = os.path.join(args.output, 'vali.txt')
     write_allrank_data(path, out['qids'], out['label'], out['feat_idx'], out['q_src'], out['q_tgt'], out['feat_tgt'])
 
 
-def build_split(split_dstore_path, split_dstore_size, lookup_path, lookup_k, k, n, shuffle=True, balance=False):
+def build_split(split_dstore_path, split_dstore_size, lookup_path, lookup_k, k, n, shuffle=True, balance=False, should_filter_top=False):
     split_dstore = Dstore(split_dstore_path, split_dstore_size, 1024)
     split_dstore.initialize(has_row=True)
     split_dstore.add_neighbors(lookup_path, lookup_k)
+
+    vocab = Dictionary()
+    vocab.add_from_file(args.vocab)
+    vocab.finalize()
 
     index = np.arange(split_dstore.tgts.shape[0])
     if shuffle:
@@ -43,6 +49,24 @@ def build_split(split_dstore_path, split_dstore_size, lookup_path, lookup_k, k, 
     qids = split_dstore.row[index]
     size = qids.shape[0]
     label = (knn_tgts == tgts.reshape(-1, 1, 1)).astype(np.int)
+
+    if should_filter_top and args.filter_top > 0:
+        print('FILTER TOP {}'.format(args.filter_top))
+        # assumes vocab is sorted desc after special tokens
+        tf = np.array(vocab.count)
+        tgts_tf = tf[tgts]
+        top_N_abs_tf_lim = tf[np.argmax(tf) + args.filter_top]
+        mask = (tgts_tf < top_N_abs_tf_lim).reshape(-1)
+        print('will filter {} / {}'.format(mask.sum(), mask.shape[0]))
+
+        src = src[mask]
+        tgts = tgts[mask]
+        knns = knns[mask]
+        dist = dist[mask]
+        knn_tgts = knn_tgts[mask]
+        qids = qids[mask]
+        size = qids.shape[0]
+        label = (knn_tgts == tgts.reshape(-1, 1, 1)).astype(np.int)
 
     if balance:
         npos = 10
@@ -167,6 +191,184 @@ def write_allrank_data(path, qids, label, feat_idx, query_src, query_tgt, feat_t
                 f.write('\n')
 
 
+class Dictionary(object):
+    """
+    A mapping from symbols to consecutive integers.
+
+    Taken from fairseq repo.
+    """
+
+    def __init__(
+        self,
+        pad="<pad>",
+        eos="</s>",
+        unk="<unk>",
+        bos="<s>",
+        extra_special_symbols=None,
+    ):
+        self.unk_word, self.pad_word, self.eos_word = unk, pad, eos
+        self.symbols = []
+        self.count = []
+        self.indices = {}
+        self.bos_index = self.add_symbol(bos)
+        self.pad_index = self.add_symbol(pad)
+        self.eos_index = self.add_symbol(eos)
+        self.unk_index = self.add_symbol(unk)
+        if extra_special_symbols:
+            for s in extra_special_symbols:
+                self.add_symbol(s)
+        self.nspecial = len(self.symbols)
+
+    def __eq__(self, other):
+        return self.indices == other.indices
+
+    def __getitem__(self, idx):
+        if idx < len(self.symbols):
+            return self.symbols[idx]
+        return self.unk_word
+
+    def __len__(self):
+        """Returns the number of symbols in the dictionary"""
+        return len(self.symbols)
+
+    def __contains__(self, sym):
+        return sym in self.indices
+
+    def index(self, sym):
+        """Returns the index of the specified symbol"""
+        assert isinstance(sym, str)
+        if sym in self.indices:
+            return self.indices[sym]
+        return self.unk_index
+
+    def unk_string(self, escape=False):
+        """Return unknown string, optionally escaped as: <<unk>>"""
+        if escape:
+            return "<{}>".format(self.unk_word)
+        else:
+            return self.unk_word
+
+    def add_symbol(self, word, n=1):
+        """Adds a word to the dictionary"""
+        if word in self.indices:
+            idx = self.indices[word]
+            self.count[idx] = self.count[idx] + n
+            return idx
+        else:
+            idx = len(self.symbols)
+            self.indices[word] = idx
+            self.symbols.append(word)
+            self.count.append(n)
+            return idx
+
+    def update(self, new_dict):
+        """Updates counts from new dictionary."""
+        for word in new_dict.symbols:
+            idx2 = new_dict.indices[word]
+            if word in self.indices:
+                idx = self.indices[word]
+                self.count[idx] = self.count[idx] + new_dict.count[idx2]
+            else:
+                idx = len(self.symbols)
+                self.indices[word] = idx
+                self.symbols.append(word)
+                self.count.append(new_dict.count[idx2])
+
+    def finalize(self, threshold=-1, nwords=-1, padding_factor=8):
+        """Sort symbols by frequency in descending order, ignoring special ones.
+
+        Args:
+            - threshold defines the minimum word count
+            - nwords defines the total number of words in the final dictionary,
+                including special symbols
+            - padding_factor can be used to pad the dictionary size to be a
+                multiple of 8, which is important on some hardware (e.g., Nvidia
+                Tensor Cores).
+        """
+        if nwords <= 0:
+            nwords = len(self)
+
+        new_indices = dict(zip(self.symbols[: self.nspecial], range(self.nspecial)))
+        new_symbols = self.symbols[: self.nspecial]
+        new_count = self.count[: self.nspecial]
+
+        c = collections.Counter(
+            dict(
+                sorted(zip(self.symbols[self.nspecial :], self.count[self.nspecial :]))
+            )
+        )
+        for symbol, count in c.most_common(nwords - self.nspecial):
+            if count >= threshold:
+                new_indices[symbol] = len(new_symbols)
+                new_symbols.append(symbol)
+                new_count.append(count)
+            else:
+                break
+
+        assert len(new_symbols) == len(new_indices)
+
+        self.count = list(new_count)
+        self.symbols = list(new_symbols)
+        self.indices = new_indices
+
+        self.pad_to_multiple_(padding_factor)
+
+    def pad_to_multiple_(self, padding_factor):
+        """Pad Dictionary size to be a multiple of *padding_factor*."""
+        if padding_factor > 1:
+            i = 0
+            while len(self) % padding_factor != 0:
+                symbol = "madeupword{:04d}".format(i)
+                self.add_symbol(symbol, n=0)
+                i += 1
+
+    def bos(self):
+        """Helper to get index of beginning-of-sentence symbol"""
+        return self.bos_index
+
+    def pad(self):
+        """Helper to get index of pad symbol"""
+        return self.pad_index
+
+    def eos(self):
+        """Helper to get index of end-of-sentence symbol"""
+        return self.eos_index
+
+    def unk(self):
+        """Helper to get index of unk symbol"""
+        return self.unk_index
+
+    def add_from_file(self, f):
+        """
+        Loads a pre-existing dictionary from a text file and adds its symbols
+        to this instance.
+        """
+        if isinstance(f, str):
+            try:
+                with open(f, "r", encoding="utf-8") as fd:
+                    self.add_from_file(fd)
+            except FileNotFoundError as fnfe:
+                raise fnfe
+            except UnicodeError:
+                raise Exception(
+                    "Incorrect encoding detected in {}, please "
+                    "rebuild the dataset".format(f)
+                )
+            return
+
+        for line in f.readlines():
+            idx = line.rfind(" ")
+            if idx == -1:
+                raise ValueError(
+                    "Incorrect dictionary format, expected '<token> <cnt>'"
+                )
+            word = line[:idx]
+            count = int(line[idx + 1 :])
+            self.indices[word] = len(self.symbols)
+            self.symbols.append(word)
+            self.count.append(count)
+
+
 class Dstore:
     def __init__(self, path, dstore_size=None, vec_size=None):
         self.path = path
@@ -204,6 +406,7 @@ if __name__ == '__main__':
     parser.add_argument('--tr-lookup-k', default=1024, type=int)
     parser.add_argument('--ntrain', default=5000, type=int)
     parser.add_argument('--tr-noshuffle', action='store_true')
+    parser.add_argument('--tr-balance', action='store_true')
     # dstore-va
     parser.add_argument('--va-dstore', default='from_dstore_valid/va', type=str)
     parser.add_argument('--va-dstore-size', default=10000, type=int)
@@ -212,6 +415,8 @@ if __name__ == '__main__':
     parser.add_argument('--nvalid', default=10000, type=int)
     parser.add_argument('--va-shuffle', action='store_true')
     # allrank
+    parser.add_argument('--filter-top', default=-1, type=int, help='If > 1, then removes any example with target in the top N term freqs.')
+    parser.add_argument('--vocab', default='data-bin/wikitext-103/dict.txt', type=str)
     parser.add_argument('--output', default=None, type=str)
     parser.add_argument('--seed', default=121, type=int)
     parser.add_argument('--k', default=32, type=int)
