f4d8e8820ef4a827e5db743ca42914ddafd3e8c5
Andrew Drozdov <andrew@mrdrozdov.com>
2021-05-03 14:36:52 -0400

    Add official train/test split.

--- offline_eval_predict_freq.py
+++ offline_eval_predict_freq.py
@@ -50,40 +50,18 @@ class Experiment:
 
     def run(self, context):
         args = context['args']
-        vocab = context['vocab']
-        knns = context['knns']
-        knn_tgts = context['knn_tgts']
-        tgts = context['tgts']
-        src = context['src']
-        dstore = context['dstore']
-
-        vocab_size = len(vocab)
-        data_size = knns.reshape(-1).shape[0]
 
         t = args.vocab_threshold
 
-        n = tgts.shape[0]
-        mask_is_freq = tgts <= t
-        n_is_freq = mask_is_freq.sum()
-        pct_is_freq = n_is_freq / n
+        trn_feat = context['train']['keys']
+        trn_y = context['train']['tgts'] <= t
+        trn_y = trn_y.astype(np.int).reshape(-1)
 
-        print('is_freq = {}/{} ({:.3f})'.format(n_is_freq, n, pct_is_freq))
-
-        feat = npy_copy(dstore.keys) # queries
-        scaler = sklearn.preprocessing.StandardScaler().fit(feat)
-        #feat = scaler.transform(feat)
-        if args.limit > 0:
-            feat = feat[:args.limit]
-        y = mask_is_freq.astype(np.int).flatten()
-        assert feat.shape[0] == y.shape[0]
-
-        piv = int(n * 0.5)
-
-        trn_feat, trn_y = feat[:piv], y[:piv]
-        tst_feat, tst_y = feat[piv:], y[piv:]
+        tst_feat = context['test']['keys']
+        tst_y = context['test']['tgts'] <= t
+        tst_y = tst_y.astype(np.int).reshape(-1)
 
         solver = 'liblinear'
-        #solver = 'lbfgs'
         model = sklearn.linear_model.LogisticRegression(max_iter=200, verbose=1, solver=solver)
         model.fit(trn_feat, trn_y)
 
@@ -95,26 +73,17 @@ class Experiment:
         print('pos_n = {}, neg_n = {}'.format(np.sum(tst_y==1), np.sum(tst_y==0)))
         print('pos_acc = {:.3f}, neg_acc = {:.3f}'.format(pos_acc, neg_acc))
 
-        mask_test = np.arange(n) >= piv
-        mask_pos_pred = mask_test.copy()
-        mask_pos_pred[piv:] = np.logical_and(mask_test[piv:], y_pred == 1)
-        mask_neg_pred = mask_test.copy()
-        mask_neg_pred[piv:] = np.logical_and(mask_test[piv:], y_pred == 0)
-        mask_pos_gold= mask_test.copy()
-        mask_pos_gold[piv:] = np.logical_and(mask_test[piv:], tst_y == 1)
-        mask_neg_gold = mask_test.copy()
-        mask_neg_gold[piv:] = np.logical_and(mask_test[piv:], tst_y == 0)
+        mask_pos_pred = y_pred == 1
+        mask_neg_pred = y_pred == 0
+        mask_pos_gold = tst_y == 1
+        mask_neg_gold = tst_y == 0
 
-
-        context['mask_test'] = mask_test
-        context['mask_pos_pred'] = mask_pos_pred
-        context['mask_neg_pred'] = mask_neg_pred
-        context['mask_pos_gold'] = mask_pos_gold
-        context['mask_neg_gold'] = mask_neg_gold
-
-
-
-        print('done')
+        out = {}
+        out['mask_pos_pred'] = mask_pos_pred
+        out['mask_neg_pred'] = mask_neg_pred
+        out['mask_pos_gold'] = mask_pos_gold
+        out['mask_neg_gold'] = mask_neg_gold
+        return out
 
 
 def npy_copy(x):
@@ -123,53 +92,88 @@ def npy_copy(x):
     return out
 
 
+class DatasetUtils:
+    def build_context(self, args):
+        if args.test:
+            dstore = Dstore(args.dstore, args.dstore_size, 1024)
+            dstore.initialize(include_keys=True)
+            dstore.add_neighbors(args.lookup, args.lookup_k)
+            dstore.add_exact(args.lookup, args.lookup_k)
+
+            train = {}
+            train['p'] = npy_copy(dstore.prob[:])
+            train['dist'] = -npy_copy(dstore.exact[:])
+            train['tgts'] = npy_copy(dstore.tgts[:])
+            train['knn_tgts'] = npy_copy(dstore.knn_tgts[:])
+            train['knns'] = npy_copy(dstore.knns[:])
+            train['keys'] = npy_copy(dstore.keys[:])
+
+            dstore = Dstore(args.test_dstore, args.test_dstore_size, 1024)
+            dstore.initialize(include_keys=True)
+            dstore.add_neighbors(args.test_lookup, args.lookup_k)
+            dstore.add_exact(args.test_lookup, args.lookup_k)
+
+            test = {}
+            test['p'] = npy_copy(dstore.prob[:])
+            test['dist'] = -npy_copy(dstore.exact[:])
+            test['tgts'] = npy_copy(dstore.tgts[:])
+            test['knn_tgts'] = npy_copy(dstore.knn_tgts[:])
+            test['knns'] = npy_copy(dstore.knns[:])
+            test['keys'] = npy_copy(dstore.keys[:])
+
+        else:
+            dstore = Dstore(args.dstore, args.dstore_size, 1024)
+            dstore.initialize(include_keys=True)
+            dstore.add_neighbors(args.lookup, args.lookup_k)
+            dstore.add_exact(args.lookup, args.lookup_k)
+
+            p = npy_copy(dstore.prob[:])
+            dist = -npy_copy(dstore.exact[:])
+            tgts = npy_copy(dstore.tgts[:])
+            knn_tgts = npy_copy(dstore.knn_tgts[:])
+            knns = npy_copy(dstore.knns[:])
+            keys = npy_copy(dstore.keys[:])
+
+            limit = args.limit
+            if limit > 0:
+                p = p[:limit]
+                dist = dist[:limit]
+                tgts = tgts[:limit]
+                knn_tgts = knn_tgts[:limit]
+                knns = knns[:limit]
+                keys = keys[:limit]
+
+            piv = int(0.5 * tgts.shape[0])
+
+            train = {}
+            train['p'] = p[:piv]
+            train['dist'] = dist[:piv]
+            train['tgts'] = tgts[:piv]
+            train['knn_tgts'] = knn_tgts[:piv]
+            train['knns'] = knns[:piv]
+            train['keys'] = keys[:piv]
+
+            test = {}
+            test['p'] = p[piv:]
+            test['dist'] = dist[piv:]
+            test['tgts'] = tgts[piv:]
+            test['knn_tgts'] = knn_tgts[piv:]
+            test['knns'] = knns[piv:]
+            test['keys'] = keys[piv:]
+
+        context = {}
+        context['train'] = train
+        context['test'] = test
+        return context
+
+
 def main(args):
+    context = DatasetUtils().build_context(args)
+
+    #
     knn_dstore = Dstore(args.knn_dstore, args.knn_dstore_size, 1024)
     knn_dstore.initialize(include_keys=False)
-
-    dstore = Dstore(args.dstore, args.dstore_size, 1024)
-    dstore.initialize(include_keys=True)
-    dstore.add_neighbors(args.lookup, args.lookup_k)
-    dstore.add_exact(args.lookup, args.lookup_k)
-    #dstore.add_annotations(args.dstore)
-
-    p = dstore.prob[:].copy()
-    dist = -dstore.exact[:].copy()
-    tgts = npy_copy(dstore.tgts[:])
-    src = tgts.copy()
-    src[1:] = tgts[:-1]
-    knn_tgts = npy_copy(dstore.knn_tgts[:, :args.k])
-    knns = npy_copy(dstore.knns[:, :args.k])
-
-    #
-    limit = args.limit
-    if limit > 0:
-        p = p[:limit]
-        dist = dist[:limit]
-        tgts = tgts[:limit]
-        knn_tgts = knn_tgts[:limit]
-        knns = knns[:limit]
-
-    #
-    label = (knn_tgts == tgts.reshape(-1, 1, 1)).astype(np.int)
-
-    #
-    knn_p = EvalUtil.get_knn_log_prob(tgts, dist, knn_tgts)
-    flat_knn_logp, flat_knn_p = EvalUtil.get_knn_probmass(tgts, dist, knn_tgts)
-
-    coeff = 0.25
-    p_ = torch.from_numpy(p).float()
-    knn_p_ = torch.from_numpy(knn_p).float()
-    new_p = EvalUtil.combine_knn_and_vocab_probs(
-                knn_p_,
-                p_,
-                coeff)
-    ppl = EvalUtil.eval_ppl(p)
-    new_ppl = EvalUtil.eval_ppl(new_p)
-
-    print('ppl = {:.3f}, knn_ppl = {:.3f}'.format(ppl, new_ppl))
-
-    #accum = np.zeros((vocab_size, 1), dtype=np.float32)
+    context['knn_dstore'] = knn_dstore
 
     #
     print('read vocab')
@@ -178,19 +182,13 @@ def main(args):
     vocab.finalize()
     print('found {} tokens'.format(len(vocab)))
     print('')
-
-    # Demo.
-    context = {}
-    context['args'] = args
     context['vocab'] = vocab
-    context['knns'] = knns
-    context['tgts'] = tgts
-    context['src'] = src
-    context['dstore'] = dstore
-    context['knn_tgts'] = knn_tgts
-    context['knn_dstore'] = knn_dstore
 
-    Experiment().run(context)
+    # Add more.
+    context['args'] = args
+
+    #
+    out = Experiment().run(context)
 
     def run_eval(context):
         knns = context['knns']
@@ -240,15 +238,21 @@ def main(args):
         mask_ppl = EvalUtil.eval_ppl(mask_p)
         print('ppl = {:.3f}, knn_ppl = {:.3f}, mask_ppl = {:.3f}'.format(ppl, new_ppl, mask_ppl))
 
-    keys = ['mask_test', 'mask_pos_gold', 'mask_neg_gold']
-
+    # Unpack test data.
+    knns = context['test']['knns']
+    knn_tgts = context['test']['knn_tgts']
+    tgts = context['test']['tgts']
+    dist = context['test']['dist']
+    p = context['test']['p']
     context_ = context
+
+    keys = ['mask_pos_gold', 'mask_neg_gold']
 
     for k in keys:
         header = 'EVAL = {}'.format(k)
         print(header)
         print('-' * len(header))
-        mask = context_[k]
+        mask = out[k]
         context = {}
         context['knns'] = knns[mask]
         context['knn_tgts'] = knn_tgts[mask]
@@ -258,13 +262,13 @@ def main(args):
         run_eval(context)
 
         print('')
-    mask = context_['mask_test']
+
     context = {}
-    context['knns'] = knns[mask]
-    context['knn_tgts'] = knn_tgts[mask]
-    context['tgts'] = tgts[mask]
-    context['dist'] = dist[mask]
-    context['p'] = p[mask]
+    context['knns'] = knns
+    context['knn_tgts'] = knn_tgts
+    context['tgts'] = tgts
+    context['dist'] = dist
+    context['p'] = p
 
     # Overall with PRED and GOLD
     header = 'OVERALL'
@@ -272,11 +276,11 @@ def main(args):
     print('-' * len(header))
 
     print('EVAL PRED')
-    m = context_['mask_pos_pred'][mask]
+    m = out['mask_pos_pred']
     run_eval_mix(context, m, skip_pos=True)
 
     print('EVAL GOLD')
-    m = context_['mask_pos_gold'][mask]
+    m = out['mask_pos_gold']
     run_eval_mix(context, m, skip_pos=True)
 
     # Subset with PRED and GOLD
@@ -284,7 +288,7 @@ def main(args):
     print(header)
     print('-' * len(header))
 
-    mask = np.logical_and(context_['mask_test'], context_['mask_pos_gold'])
+    mask = out['mask_pos_gold']
     context = {}
     context['knns'] = knns[mask]
     context['knn_tgts'] = knn_tgts[mask]
@@ -293,11 +297,11 @@ def main(args):
     context['p'] = p[mask]
 
     print('EVAL PRED')
-    m = context_['mask_pos_pred'][mask]
+    m = out['mask_pos_pred'][mask]
     run_eval_mix(context, m, skip_pos=True)
 
     print('EVAL GOLD')
-    m = context_['mask_pos_gold'][mask]
+    m = out['mask_pos_gold'][mask]
     run_eval_mix(context, m, skip_pos=True)
 
     #
@@ -305,7 +309,7 @@ def main(args):
     print(header)
     print('-' * len(header))
 
-    mask = np.logical_and(context_['mask_test'], context_['mask_neg_gold'])
+    mask = out['mask_neg_gold']
     context = {}
     context['knns'] = knns[mask]
     context['knn_tgts'] = knn_tgts[mask]
@@ -314,11 +318,11 @@ def main(args):
     context['p'] = p[mask]
 
     print('EVAL PRED')
-    m = context_['mask_pos_pred'][mask]
+    m = out['mask_pos_pred'][mask]
     run_eval_mix(context, m, skip_pos=True)
 
     print('EVAL GOLD')
-    m = context_['mask_pos_gold'][mask]
+    m = out['mask_pos_gold'][mask]
     run_eval_mix(context, m, skip_pos=True)
 
 
@@ -328,6 +332,10 @@ if __name__ == '__main__':
     parser.add_argument('--dstore', default='dstore_valid', type=str)
     parser.add_argument('--dstore-size', default=217646, type=int)
     parser.add_argument('--vocab', default='data-bin/wikitext-103/dict.txt')
+    #
+    parser.add_argument('--test-dstore', default='dstore_test', type=str)
+    parser.add_argument('--test-dstore-size', default=245569, type=int)
+    parser.add_argument('--test-lookup', default='dstore_test/lookup')
     # dstore neighbors
     parser.add_argument('--lookup', default='dstore_valid/lookup', type=str)
     parser.add_argument('--lookup-k', default=1024, type=int)
@@ -340,6 +348,7 @@ if __name__ == '__main__':
     parser.add_argument('--vocab_threshold', default=1000, type=int)
     # debug
     parser.add_argument('--limit', default=-1, type=int)
+    parser.add_argument('--test', action='store_true')
     args = parser.parse_args()
 
     # Print flags.
