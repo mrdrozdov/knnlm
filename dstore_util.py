fdf6db22d3e78f9dc7f755dfe911b00540573641
Andrew Drozdov <andrew@mrdrozdov.com>
2021-05-03 13:01:42 -0400

    Add util scripts.

---
+++ dstore_util.py
@@ -0,0 +1,65 @@
+import numpy as np
+import json
+import os
+
+
+class FilteredDstore:
+    def __init__(self, path, vec_size=1024):
+        self.path = path
+        self.vec_size = vec_size
+
+    def initialize(self):
+        path = self.path
+
+        # Read metadata.
+        with open(os.path.join(path, 'info.json')) as f:
+            metadata = json.loads(f.read())
+
+        n_keep = metadata['keep']
+
+        self.keys = np.memmap(os.path.join(path, 'dstore_keys.npy'), dtype=np.float32, mode='r', shape=(n_keep, self.vec_size))
+        self.keep_ids = np.memmap(os.path.join(path, 'keep_ids.npy'), dtype=np.int, mode='r', shape=(n_keep, 1))
+        self.disc_ids = np.memmap(os.path.join(path, 'discard_ids.npy'), dtype=np.int, mode='r', shape=(n_keep, 1))
+
+
+
+class Dstore:
+    def __init__(self, path, dstore_size=None, vec_size=None):
+        self.path = path
+        self.dstore_size = dstore_size
+        self.vec_size = vec_size
+        self._initialized = False
+
+    def initialize(self, include_keys=False):
+        path = self.path
+        if include_keys:
+            self.keys = np.memmap(os.path.join(path, 'dstore_keys.npy'), dtype=np.float32, mode='r', shape=(self.dstore_size, self.vec_size))
+        self.tgts = np.memmap(os.path.join(path, 'dstore_tgts.npy'), dtype=np.int, mode='r', shape=(self.dstore_size, 1))
+        self.vals = np.memmap(os.path.join(path, 'dstore_vals.npy'), dtype=np.int, mode='r', shape=(self.dstore_size, 1))
+        self.prob = np.memmap(os.path.join(path, 'dstore_prob.npy'), dtype=np.float32, mode='r', shape=(self.dstore_size, 1))
+        self._initialized = True
+
+    def add_neighbors(self, path, k):
+        self.knns = np.memmap(os.path.join(path, 'lookup_knns.npy'), dtype=np.int, mode='r', shape=(self.dstore_size, k, 1))
+        self.knn_tgts = np.memmap(os.path.join(path, 'lookup_knn_tgts.npy'), dtype=np.int, mode='r', shape=(self.dstore_size, k, 1))
+        self.dist = np.memmap(os.path.join(path, 'lookup_dist.npy'), dtype=np.float32, mode='r', shape=(self.dstore_size, k, 1))
+        # self.lookup_done = np.memmap(os.path.join(path, 'lookup_done.npy'), dtype=np.int, mode='r', shape=(self.dstore_size, 1))
+
+    def add_exact(self, path, k):
+        self.exact = np.memmap(os.path.join(path, 'lookup_exact.npy'), dtype=np.float32, mode='r', shape=(self.dstore_size, k, 1))
+
+    def add_annotations(self, path):
+        self.src_pos = np.memmap(os.path.join(path, 'annotation_src_pos.npy'), dtype=np.int, mode='r', shape=(self.dstore_size, 1))
+
+        # read pos dict
+        self.idx2tag = []
+        print('Reading POS Vocab...')
+        path_pos_dict = os.path.join(path, 'pos_dict.txt')
+        with open(path_pos_dict) as f:
+            for line in f:
+                print(line.strip())
+                idx, sym, _ = line.strip().split()
+                self.idx2tag.append(sym)
+        self.tag2idx = {v: k for k, v in enumerate(self.idx2tag)}
+        print('done\n')
+
---
+++ eval_util.py
@@ -0,0 +1,46 @@
+import torch
+import numpy as np
+
+
+class EvalUtil:
+    @staticmethod
+    def get_knn_probmass(tgts, dists, knn_tgts):
+        tgts = torch.from_numpy(tgts).long().view(-1)
+        dists = torch.from_numpy(dists).float().squeeze(-1)
+        probs = torch.log_softmax(dists, dim=-1)
+        mass = torch.exp(probs)
+        return probs, mass
+
+    @staticmethod
+    def get_knn_log_prob(tgts, dists, knn_tgts):
+
+        tgts = torch.from_numpy(tgts).long().view(-1)
+        dists = torch.from_numpy(dists).float().squeeze(-1)
+        #dists = -dists
+        probs = torch.log_softmax(dists, dim=-1)
+
+        index_mask = torch.eq(torch.from_numpy(knn_tgts).long().squeeze(-1), tgts.unsqueeze(-1)).float()
+        index_mask[index_mask == 0] = -10000 # for stability
+        index_mask[index_mask == 1] = 0
+
+        # (T_reducedxB)
+        yhat_knn_prob = torch.logsumexp(probs + index_mask, dim=-1).clone().numpy()
+
+        # Bx1
+        return yhat_knn_prob.reshape(-1, 1)
+
+    @staticmethod
+    def combine_knn_and_vocab_probs(knn_p, vocab_p, coeff):
+        combine_probs = torch.stack([vocab_p, knn_p], dim=0)
+        coeffs = torch.ones_like(combine_probs)
+        coeffs[0] = np.log(1 - coeff)
+        coeffs[1] = np.log(coeff)
+        curr_prob = torch.logsumexp(combine_probs + coeffs, dim=0)
+
+        return curr_prob
+
+    @staticmethod
+    def eval_ppl(p):
+        avg_nll = -p.mean() / np.log(2)
+        ppl = 2**avg_nll
+        return ppl
