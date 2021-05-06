da27bec26b015d24005f28e57f173d96f23bc7e7
Andrew Drozdov <andrew@mrdrozdov.com>
2021-05-06 12:22:04 -0400

    Bi-gram LM is not the best.

--- offline_eval_ngram_lm.py
+++ offline_eval_ngram_lm.py
@@ -30,9 +30,9 @@ class Model:
         return p
 
     def predict(self, w0, w1):
-        if self.c[1][w1] == 0:
+        if self.c[1][w0] == 0:
             return 0
-        return self.c[2][(w0, w1)] / self.c[1][w1]
+        return self.c[2][(w0, w1)] / self.c[1][w0]
 
 
 def build_ngram_lm(trn_src, trn_tgt, limit=-1):
@@ -78,16 +78,18 @@ def main(args):
     trn_tgt = npy_copy(knn_dstore.tgts[:]).reshape(-1)
     trn_src = trn_tgt.copy()
     trn_src[1:] = trn_src[:-1]
-    ngram_lm = build_ngram_lm(trn_src, trn_tgt)
+    ngram_lm = build_ngram_lm(trn_src, trn_tgt, limit=args.ngram_limit)
 
     print('read vocab')
     vocab = Dictionary()
     vocab.add_from_file(args.vocab)
     vocab.finalize()
+    vocab_size = len(vocab)
     print('found {} tokens'.format(len(vocab)))
     print('')
 
     def run_eval(context):
+        coeff = context['coeff']
         knns = context['knns']
         knn_tgts = context['knn_tgts']
         src = context['src']
@@ -102,13 +104,25 @@ def main(args):
         ngram_p = ngram_lm.batch_predict(src, tgts)
         ngram_logp = np.log(np.clip(ngram_p, 1e-8, 1))
         ngram_logp[ngram_p == 0] = -10000
+        del ngram_p
 
-        coeff = 0.25
         p_ = torch.from_numpy(p).float()
         knn_p_ = torch.from_numpy(knn_p).float()
         ngram_p_ = torch.from_numpy(ngram_logp).float()
 
+        unif_p_ = p_.clone()
+        unif_p_[:] = np.log(1/vocab_size)
+
         #
+        unif_knn_p = EvalUtil.combine_knn_and_vocab_probs(
+                    knn_p_,
+                    unif_p_,
+                    0.9)
+        unif_ngram_p = EvalUtil.combine_knn_and_vocab_probs(
+                    ngram_p_,
+                    unif_p_,
+                    0.9)
+
         interp_knn_p = EvalUtil.combine_knn_and_vocab_probs(
                     knn_p_,
                     p_,
@@ -125,18 +139,16 @@ def main(args):
                 )
 
         ppl = EvalUtil.eval_ppl(p)
-        #knn_ppl = EvalUtil.eval_ppl(knn_p_)
-        #ngram_ppl = EvalUtil.eval_ppl(ngram_p_)
-        knn_ppl = 0
-        ngram_ppl = 0
+        knn_ppl = EvalUtil.eval_ppl(unif_knn_p)
+        ngram_ppl = EvalUtil.eval_ppl(unif_ngram_p)
         interp_knn_ppl = EvalUtil.eval_ppl(interp_knn_p)
         interp_ngram_ppl = EvalUtil.eval_ppl(interp_ngram_p)
         interp_many_ppl = EvalUtil.eval_ppl(interp_many_p)
 
         n = tgts.shape[0]
 
-        print('n = {}, ppl = {:.3f}, knn_ppl = {:.3f}, ngram_ppl = {:.3f}, i_knn_ppl = {:.3f}, i_ngram_ppl = {:.3f}, i_many_ppl = {:.3f}'.format(
-            n, ppl, knn_ppl, ngram_ppl, interp_knn_ppl, interp_ngram_ppl, interp_many_ppl))
+        print('n = {}, coeff = {:.3f}, ppl = {:.3f}, knn_ppl = {:.3f}, ngram_ppl = {:.3f}, i_knn_ppl = {:.3f}, i_ngram_ppl = {:.3f}, i_many_ppl = {:.3f}'.format(
+            n, coeff, ppl, knn_ppl, ngram_ppl, interp_knn_ppl, interp_ngram_ppl, interp_many_ppl))
 
     def print_header(x, l=8, n=40):
         line = '-' * l
@@ -147,6 +159,7 @@ def main(args):
     # COMPUTE ALL EVAL
     print_header('EVAL ALL')
     context = {}
+    context['coeff'] = 0.25
     context['knns'] = knns
     context['knn_tgts'] = knn_tgts
     context['src'] = src
@@ -154,6 +167,19 @@ def main(args):
     context['dist'] = dist
     context['p'] = p
     run_eval(context)
+    print('')
+
+    print_header('many coeff')
+    coeff_list = np.arange(1, 10) / 20
+    for coeff in coeff_list:
+        context['coeff'] = coeff
+        context['knns'] = knns
+        context['knn_tgts'] = knn_tgts
+        context['src'] = src
+        context['tgts'] = tgts
+        context['dist'] = dist
+        context['p'] = p
+        run_eval(context)
     print('')
 
     # COMPUTE FILTERED EVAL
@@ -175,6 +201,7 @@ def main(args):
         print_header('interval = {}:{}'.format(t0, t1))
 
         context = {}
+        context['coeff'] = 0.25
         context['knns'] = knns[mask]
         context['knn_tgts'] = knn_tgts[mask]
         context['src'] = src[mask]
@@ -196,6 +223,7 @@ def main(args):
         print_header('interval = {}:{}'.format(t0, t1))
 
         context = {}
+        context['coeff'] = 0.25
         context['knns'] = knns[mask]
         context['knn_tgts'] = knn_tgts[mask]
         context['src'] = src[mask]
@@ -223,6 +251,7 @@ def main(args):
             print_header('interval = src:{}:{} tgt:{}:{}'.format(t0_src, t1_src, t0_tgt, t1_tgt))
 
             context = {}
+            context['coeff'] = 0.25
             context['knns'] = knns[mask]
             context['knn_tgts'] = knn_tgts[mask]
             context['src'] = src[mask]
@@ -257,6 +286,7 @@ if __name__ == '__main__':
     parser.add_argument('--k', default=1024, type=int)
     # debug
     parser.add_argument('--limit', default=-1, type=int)
+    parser.add_argument('--ngram-limit', default=-1, type=int)
     parser.add_argument('--preset', default='valid', type=str)
     parser.add_argument('--approx', action='store_true')
     args = parser.parse_args()
