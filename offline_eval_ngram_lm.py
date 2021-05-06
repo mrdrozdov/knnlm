1ca8d1ee6319ed2f5c6df85f3e1b9a6e5bb2f544
Andrew Drozdov <andrew@mrdrozdov.com>
2021-05-06 13:30:19 -0400

    Use bigger window size with smoothing.

--- offline_eval_ngram_lm.py
+++ offline_eval_ngram_lm.py
@@ -17,43 +17,74 @@ from tqdm import tqdm
 
 
 class Model:
-    def __init__(self, c):
+    def __init__(self, c, max_ngram=3):
         self.c = c
+        self.c1_sum = sum(c[1].values())
+        self.max_ngram = max_ngram
 
-    def batch_predict(self, w0, w1):
-        w0 = w0.reshape(-1).tolist()
-        w1 = w1.reshape(-1).tolist()
-        assert len(w0) == len(w1)
-        p = np.zeros((len(w1), 1), dtype=np.float32)
-        for i, (w0_, w1_) in enumerate(zip(w0, w1)):
-            p[i] = self.predict(w0_, w1_)
+    def batch_predict(self, src, tgts):
+        # TODO: Should we be able to use small max_ngram as desired?
+        max_ngram = self.max_ngram
+        assert src.shape == tgts.shape
+        n = tgts.shape[0]
+        p = np.zeros((n, 1), dtype=np.float32)
+        for i in range(n):
+            tgt = tgts[i].item()
+            context = tgts[i-max_ngram+1:i].flatten().tolist()
+            p[i] = self.predict(context, tgt)
         return p
 
-    def predict(self, w0, w1):
-        if self.c[1][w0] == 0:
-            return 0
-        return self.c[2][(w0, w1)] / self.c[1][w0]
+    def predict(self, context, tgt, coeff_list=[0.3, 0.4, 0.3]):
+        p = 0
+        max_ngram = len(context) + 1
+
+        for size in range(1, max_ngram + 1):
+            if size == 1:
+                p_ = self.c[size][tgt] / self.c1_sum
+            elif size - 1 > len(context):
+                raise Exception('size = {}, len(context) = {}'.format(size, len(context)))
+            else:
+                top_key = tuple(context[-(size-1):] + [tgt])
+                if size == 2:
+                    bot_key = context[-1]
+                else:
+                    bot_key = tuple(context[-(size-1):])
+                bot = self.c[size - 1][bot_key]
+                if bot > 0:
+                    p_ = self.c[size][top_key] / bot
+                else:
+                    p_ = 0
+
+            coeff = coeff_list[-size]
+            p += coeff * p_
+
+        return p
 
 
-def build_ngram_lm(trn_src, trn_tgt, limit=-1):
-    bigram = collections.Counter()
-    unigram = collections.Counter()
+def build_ngram_lm(trn_src, trn_tgt, limit=-1, max_ngram=3):
 
     n = trn_tgt.shape[0]
     if limit > 0:
         n = int(limit)
 
-    for i in tqdm(range(n), desc='train'):
-        w0 = trn_src[i].item()
-        w1 = trn_tgt[i].item()
-        bigram[(w0, w1)] += 1
-        unigram[w1] += 1
-
     c = {}
-    c[1] = unigram
-    c[2] = bigram
 
-    model = Model(c=c)
+    for size in range(1, max_ngram + 1):
+        c[size] = collections.Counter()
+
+    for t in tqdm(range(100, n), desc='train'):
+
+        win = trn_tgt[t-max_ngram+1:t+1].flatten().tolist()
+
+        for size in range(1, max_ngram + 1):
+            if size == 1:
+                w_t = win[-1]
+                c[size][w_t] += 1
+            else:
+                gram = tuple(win[-size:])
+                c[size][gram] += 1
+
+    model = Model(c=c, max_ngram=max_ngram)
     return model
 
 
@@ -78,7 +109,7 @@ def main(args):
     trn_tgt = npy_copy(knn_dstore.tgts[:]).reshape(-1)
     trn_src = trn_tgt.copy()
     trn_src[1:] = trn_src[:-1]
-    ngram_lm = build_ngram_lm(trn_src, trn_tgt, limit=args.ngram_limit)
+    ngram_lm = build_ngram_lm(trn_src, trn_tgt, limit=args.ngram_limit, max_ngram=args.max_ngram)
 
     print('read vocab')
     vocab = Dictionary()
@@ -286,7 +317,8 @@ if __name__ == '__main__':
     parser.add_argument('--k', default=1024, type=int)
     # debug
     parser.add_argument('--limit', default=-1, type=int)
-    parser.add_argument('--ngram-limit', default=-1, type=int)
+    parser.add_argument('--ngram-limit', default=-1, type=int, help='Number of training examples for ngram lm.')
+    parser.add_argument('--max-ngram', default=3, type=int, help='Token width of ngram lm.')
     parser.add_argument('--preset', default='valid', type=str)
     parser.add_argument('--approx', action='store_true')
     args = parser.parse_args()
